2191a901ab7b3c1121479c5343993a70737ac13a
LoejeeMan <72646329+GuyWithBag@users.noreply.github.com>
2025-08-06 14:02:06 +0800

    init

---
+++ index.js
@@ -0,0 +1,31 @@
+const http = require("http");
+const express = require("express");
+
+const server = http.createServer((req, res) => {
+	res.setHeader("Content-Type", "text/html");
+	let myurl = "./views";
+
+	if (req.url === "/") {
+		myurl += "/index.html";
+		res.statusCode = 200;
+	} else if (req.url === "/about") {
+		myurl += "/about.html";
+		res.statusCode = 200;
+	} else {
+		myurl += "/error.html";
+		res.statusCode = 404;
+	}
+
+	fs.readFile(myurl, (err, data) => {
+		if (err) {
+			console.log(err);
+		} else {
+			res.write(data);
+			res.end();
+		}
+	});
+});
+
+server.listen(3000, "localhost", () => {
+	console.log("listen");
+});
